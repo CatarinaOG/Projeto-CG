06145acc96fce03357a3671f13d633275fb5855f
Catarina <a93259@alunos.uminho.pt>
2022-04-02 14:17:35 +0100

    ef

---
+++ TP1/Generator/main.cpp
@@ -0,0 +1,370 @@
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <fstream>
+#include <GL/glut.h>
+#endif
+
+#define _USE_MATH_DEFINES
+#include <math.h>
+#include <string.h>
+#include <vector>
+
+using namespace std;
+
+char** arguments;
+int argumentsLen;
+
+
+void writePlane(int length, int divisions, char* fileName) {
+
+    std::ofstream MyFile(fileName);
+
+    float size = static_cast<float>(length) / divisions;
+    float startx = -(length / static_cast<float>(2));
+    float startxFixo = startx;
+    float startz = length / static_cast<float>(2);
+    float startzFixo = startz;
+
+
+    for (int j = 0; j < divisions; j++) {
+
+        startz = startzFixo - (j * size);
+
+        for (int i = 0; i < divisions; i++) {
+
+            startx = startxFixo + (i * size);
+
+            MyFile << startx << ";" << 0 << ";" << startz << ";";
+            MyFile << startx + size << ";" << 0 << ";" << startz << ";";
+            MyFile << startx + size << ";" << 0 << ";" << startz - size << std::endl;
+
+            MyFile << startx << ";" << 0 << ";" << startz << ";";
+            MyFile << startx + size << ";" << 0 << ";" << startz - size << ";";
+            MyFile << startx << ";" << 0 << ";" << startz - size << std::endl;
+        }
+    }
+
+    MyFile.close();
+}
+
+void writeCube(int length, int divisions, char* fileName) {
+
+    std::ofstream MyFile(fileName);
+
+
+    // ------------------ BASES CUBO
+
+    float size = static_cast<float>(length) / divisions; //length de cada divisao
+    float startx = -(length / static_cast<float>(2)); //x inicial de forma ao cubo centrar na origem
+    float startxFixo = startx;
+    float startz = length / static_cast<float>(2);
+    float startzFixo = startz;
+    float bottomY = -(length / static_cast<float>(2));
+    float topY = bottomY + length;
+
+
+    for (int j = 0; j < divisions; j++) {
+
+        startz = startzFixo - (j * size);
+
+        for (int i = 0; i < divisions; i++) {
+
+            startx = startxFixo + (i * size);
+
+            // ------------------ BASES 1
+
+            MyFile << startx << ";" << bottomY << ";" << startz << ";";
+            MyFile << startx + size << ";" << bottomY << ";" << startz - size << ";";
+            MyFile << startx + size << ";" << bottomY << ";" << startz << std::endl;
+
+            MyFile << startx << ";" << bottomY << ";" << startz << ";";
+            MyFile << startx << ";" << bottomY << ";" << startz - size << ";";
+            MyFile << startx + size << ";" << bottomY << ";" << startz - size << std::endl;
+
+
+            // ------------------ BASES 2
+
+            MyFile << startx << ";" << topY << ";" << startz << ";";
+            MyFile << startx + size << ";" << topY << ";" << startz << ";";
+            MyFile << startx + size << ";" << topY << ";" << startz - size << std::endl;
+
+            MyFile << startx << ";" << topY << ";" << startz << ";";
+            MyFile << startx + size << ";" << topY << ";" << startz - size << ";";
+            MyFile << startx << ";" << topY << ";" << startz - size << std::endl;
+        }
+    }
+
+    // ---------------- front left back AND right-------------------
+
+    startx = -(length / static_cast<float>(2)); //x inicial de forma ao cubo centrar na origem
+    startxFixo = startx;
+
+    startz = (length / static_cast<float>(2));
+    float endz = -(length / static_cast<float>(2));
+
+    float starty = -(length / static_cast<float>(2));
+    float startYfixo = starty;
+
+
+    for (int j = 0; j < divisions; j++) {
+
+        starty = startYfixo + (j * size);
+
+        for (int i = 0; i < divisions; i++) {
+
+            startx = startxFixo + (i * size);
+
+            //---------------------------- FACE 1
+
+            MyFile << startx << ";" << starty << ";" << startz << ";";
+            MyFile << startx + size << ";" << starty << ";" << startz << ";";
+            MyFile << startx << ";" << starty + size << ";" << startz << std::endl;
+
+            MyFile << startx << ";" << starty + size << ";" << startz << ";";
+            MyFile << startx + size << ";" << starty << ";" << startz << ";";
+            MyFile << startx + size << ";" << starty + size << ";" << startz << std::endl;
+
+
+            //---------------------------- FACE 2
+
+            MyFile << startx << ";" << starty << ";" << endz << ";";
+            MyFile << startx << ";" << starty + size << ";" << endz << ";";
+            MyFile << startx + size << ";" << starty << ";" << endz << std::endl;
+
+            MyFile << startx << ";" << starty + size << ";" << endz << ";";
+            MyFile << startx + size << ";" << starty + size << ";" << endz << ";";
+            MyFile << startx + size << ";" << starty << ";" << endz << std::endl;
+        }
+    }
+
+
+    // ------ front right / back left-------
+
+
+    startx = -(length / static_cast<float>(2));
+    float endx = startx + length;
+
+    startz = -(length / static_cast<float>(2));
+    startzFixo = startz;
+
+    starty = -(length / static_cast<float>(2));
+    startYfixo = starty;
+
+
+    for (int j = 0; j < divisions; j++) {
+        startz = startzFixo + (j * size);
+
+
+        for (int i = 0; i < divisions; i++) {
+            starty = startYfixo + (i * size);
+
+            // ------- LADO 1
+
+            MyFile << startx << ";" << starty << ";" << startz + size << ";";
+            MyFile << startx << ";" << starty + size << ";" << startz << ";";
+            MyFile << startx << ";" << starty << ";" << startz << std::endl;
+
+            MyFile << startx << ";" << starty + size << ";" << startz << ";";
+            MyFile << startx << ";" << starty << ";" << startz + size << ";";
+            MyFile << startx << ";" << starty + size << ";" << startz + size << std::endl;
+
+
+            // ------- LADO 2
+
+            MyFile << endx << ";" << starty + size << ";" << startz + size << ";";
+            MyFile << endx << ";" << starty << ";" << startz + size << ";";
+            MyFile << endx << ";" << starty + size << ";" << startz << std::endl;
+
+            MyFile << endx << ";" << starty << ";" << startz << ";";
+            MyFile << endx << ";" << starty + size << ";" << startz << ";";
+            MyFile << endx << ";" << starty << ";" << startz + size << std::endl;
+
+            
+
+        }
+    }
+
+    MyFile.close();
+}
+
+void writeCone(float radius, float height, int cone_slices, int cone_stacks, char* fileName) {
+    float px_top = 0,
+        px_bottom = 0,
+        py_top = 0,
+        py_bottom = 0,
+        pz_top = 0,
+        pz_bottom = 0,
+        angle_bottom = 0,
+        angle_top = 0,
+        starting_top_angle,
+        starting_bottom_angle;
+
+    std::ofstream MyFile(fileName);
+
+    float radius_step = radius / (float)cone_stacks;
+
+    float bottom_radius = radius;
+    float top_radius = radius - radius_step;
+
+    float stack_height = height / (float)cone_stacks;
+
+
+    float angle_step = (2 * M_PI) / (float)cone_slices;
+
+    for (int j = 0; j < cone_slices; j++) {
+
+        for (int i = 0; i < cone_stacks; i++) {
+
+            px_bottom = bottom_radius * sin(angle_bottom);
+            pz_bottom = bottom_radius * cos(angle_bottom);
+
+            MyFile << px_bottom << ";" << i * stack_height << ";" << pz_bottom << ";";
+
+
+            px_top = top_radius * sin(angle_top);
+            pz_top = top_radius * cos(angle_top);
+
+            MyFile << px_top << ";" << (i + 1) * stack_height << ";" << pz_top << ";";
+
+            angle_bottom -= angle_step;
+
+
+            px_bottom = bottom_radius * sin(angle_bottom);
+            pz_bottom = bottom_radius * cos(angle_bottom);
+
+            MyFile << px_bottom << ";" << i * stack_height << ";" << pz_bottom << std::endl;
+
+
+            MyFile << px_bottom << ";" << i * stack_height << ";" << pz_bottom << ";";
+
+            px_top = top_radius * sin(angle_top);
+            pz_top = top_radius * cos(angle_top);
+
+            MyFile << px_top << ";" << (i + 1) * stack_height << ";" << pz_top << ";";
+
+            angle_top -= angle_step;
+
+            px_top = top_radius * sin(angle_top);
+            pz_top = top_radius * cos(angle_top);
+
+            MyFile << px_top << ";" << (i + 1) * stack_height << ";" << pz_top << std::endl;
+
+            bottom_radius = top_radius;
+            top_radius -= radius_step;
+            angle_top += angle_step;
+            angle_bottom += angle_step;
+
+
+        }
+        angle_top -= angle_step;
+        angle_bottom -= angle_step;
+        bottom_radius = radius;
+        top_radius = radius - radius_step;
+    }
+
+    angle_bottom = 0;
+
+    for (int i = 0; i <= cone_slices; i++) {
+
+        px_bottom = radius * sin(angle_bottom);
+        pz_bottom = radius * cos(angle_bottom);
+        MyFile << px_bottom << ";" << 0.0 << ";" << pz_bottom << ";";
+
+        angle_bottom -= angle_step;
+
+        px_bottom = radius * sin(angle_bottom);
+        pz_bottom = radius * cos(angle_bottom);
+        MyFile << px_bottom << ";" << 0.0 << ";" << pz_bottom << ";";
+
+        MyFile << 0.0 << ";" << 0.0 << ";" << 0.0 << std::endl;
+
+    }
+}
+
+struct vertex {
+    float x, y, z;
+};
+
+vertex assignCoords(float radius, int currStack, int currSlice, float i_beta, float d_beta, float d_alpha) {
+    vertex v = {
+            radius * cos(i_beta - (currStack * d_beta)) * sin(currSlice * d_alpha),
+            radius * sin(i_beta - (currStack * d_beta)),
+            radius * cos(i_beta - (currStack * d_beta)) * cos(currSlice * d_alpha),
+    };
+    return v;
+}
+
+void writeSphere(float radius, int slices, int stacks, char* filename) {
+
+    std::ofstream Myfile(filename);
+
+    float d_alpha = (float)2 * M_PI / slices;
+    float d_beta = (float)M_PI / stacks;
+
+    float i_alpha = 0.0f;
+    float i_beta = M_PI / 2;
+
+    float x, y, z;
+
+    for (int currStack = 0; currStack < stacks; currStack++) {
+        //int currStack = 3;
+        for (int currSlice = 0; currSlice < slices; currSlice++) {
+            vertex v1 = assignCoords(radius, currStack, currSlice, i_beta, d_beta, d_alpha);
+            vertex v2 = assignCoords(radius, currStack + 1, currSlice, i_beta, d_beta, d_alpha);
+            vertex v3 = assignCoords(radius, currStack + 1, currSlice + 1, i_beta, d_beta, d_alpha);
+
+            vertex v4 = assignCoords(radius, currStack, currSlice + 1, i_beta, d_beta, d_alpha);
+
+            //1� triangulo
+
+            Myfile << v1.x << ";" << v1.y << ";" << v1.z << ";";
+            Myfile << v2.x << ";" << v2.y << ";" << v2.z << ";";
+            Myfile << v3.x << ";" << v3.y << ";" << v3.z << std::endl;
+
+            //2� triangulo
+
+            Myfile << v1.x << ";" << v1.y << ";" << v1.z << ";";
+            Myfile << v3.x << ";" << v3.y << ";" << v3.z << ";";
+            Myfile << v4.x << ";" << v4.y << ";" << v4.z << std::endl;
+
+        }
+    }
+    Myfile.close();
+
+}
+
+void choosePrimitive() {
+    if (strcmp(arguments[1], "sphere") == 0 && argumentsLen >= 4) {
+        writeSphere(atoi(arguments[2]), atoi(arguments[3]), atoi(arguments[4]), arguments[5]);
+    }
+
+    if (strcmp(arguments[1], "box") == 0 && argumentsLen >= 5) {
+        writeCube(atoi(arguments[2]), atoi(arguments[3]), arguments[4]);
+    }
+
+    if (strcmp(arguments[1], "cone") == 0 && argumentsLen >= 5) {
+        writeCone(atoi(arguments[2]), atoi(arguments[3]), atoi(arguments[4]), atoi(arguments[5]), arguments[6]);
+    }
+
+    if (strcmp(arguments[1], "plane") == 0 ) {
+        if(argumentsLen >= 5)
+            writePlane(atoi(arguments[2]), atoi(arguments[3]), arguments[4]);
+        else {
+            writePlane(atoi(arguments[2]), atoi(arguments[2]), arguments[3]);
+        }
+    }
+
+}
+
+
+int main(int argc, char** argv) {
+
+    arguments = argv;
+    argumentsLen = argc;
+
+    choosePrimitive();
+
+
+    return 1;
+}
--- TP1/engine/main.cpp
+++ TP1/engine/main.cpp
@@ -12,14 +12,10 @@
 #include <vector>
 #include <fstream>
 #include <iostream>
-#include "../tinyxml.h"
+#include "tinyxml.h"
 using namespace std;
 
 GLdouble x1, t1, z1, x2, y2, z2, x3, y3, z3, fov, near, far;
-float camX, camY, camZ;
-float alpha = 0.0f, beta = 0.0f, radius = 5.0f;
-
-
 char* filename;
 
 struct vertex {
@@ -27,64 +23,6 @@ struct vertex {
 };
 
 vector<vertex> vertices;
-vector<float> transformations;
-vector<const char*> models;
-vector< TiXmlElement*> groupStack;
-
-
-/*
-
-    1 -> translate
-    2 -> rotate
-    3 -> scale
-
-    4 -> pushMatrix
-    5 -> popMatrix
-
-    6-> model
-*/
-
-void spherical2Cartesian() {
-
-    camX = radius * cos(beta) * sin(alpha);
-    camY = radius * sin(beta);
-    camZ = radius * cos(beta) * cos(alpha);
-}
-
-
-
-
-void processSpecialKeys(int key, int xx, int yy){
-    switch(key){
-        case GLUT_KEY_RIGHT:
-            alpha -= 0.1;
-            break;
-        case GLUT_KEY_LEFT:
-            alpha += 0.1;
-            break;
-        case GLUT_KEY_UP:
-            beta += 0.1f;
-            if (beta > 1.5f)
-                beta = 1.5f;
-            break;
-        case GLUT_KEY_DOWN:
-            beta -= 0.1f;
-            if (beta < -1.5f)
-                beta = -1.5f;
-            break;
-        case GLUT_KEY_PAGE_DOWN:
-            radius -= 0.1f;
-            if(radius < 0.1f)
-                radius = 0.1f;
-            break;
-        case GLUT_KEY_PAGE_UP:
-            radius += 0.1f;
-            break;
-    }
-    spherical2Cartesian();
-    glutPostRedisplay();
-}
-
 
 
 void changeSize(int w, int h) {
@@ -94,7 +32,7 @@ void changeSize(int w, int h) {
     if (h == 0)
         h = 1;
 
-    // compute window's aspect ratio
+    // compute window's aspect ratio 
     float ratio = w * 1.0 / h;
 
     // Set the projection matrix as current
@@ -114,15 +52,15 @@ void changeSize(int w, int h) {
 
 void drawCone(float radius, float height, int cone_slices, int cone_stacks) {
     float px_top = 0,
-            px_bottom = 0,
-            py_top = 0,
-            py_bottom = 0,
-            pz_top = 0,
-            pz_bottom = 0,
-            angle_bottom = 0,
-            angle_top = 0,
-            starting_top_angle,
-            starting_bottom_angle;
+        px_bottom = 0,
+        py_top = 0,
+        py_bottom = 0,
+        pz_top = 0,
+        pz_bottom = 0,
+        angle_bottom = 0,
+        angle_top = 0,
+        starting_top_angle,
+        starting_bottom_angle;
 
 
     float radius_step = radius / (float)cone_stacks;
@@ -209,7 +147,7 @@ void drawCone(float radius, float height, int cone_slices, int cone_stacks) {
 }
 
 void tokenize(std::string const& str, const char delim,
-              std::vector<std::string>& out)
+    std::vector<std::string>& out)
 {
     size_t start;
     size_t end = 0;
@@ -222,8 +160,7 @@ void tokenize(std::string const& str, const char delim,
 }
 
 
-// parse de ficheiros .3d
-void parser(const char* filename) {
+ void parser(const char* filename) {
 
     string line;
     ifstream myfile(filename);
@@ -254,179 +191,19 @@ void parser(const char* filename) {
 }
 
 
-void drawPrimitive(){
-    glColor3f(1.0f, 1.0f, 1.0f);
+void draw(vector<vertex> vertices) {
+    int drawn = 0;
     glBegin(GL_TRIANGLES);
     for (std::size_t i = 0; i < vertices.size(); i += 3) {
         glVertex3f(vertices[i].x, vertices[i].y, vertices[i].z);
         glVertex3f(vertices[i + 1].x, vertices[i + 1].y, vertices[i + 1].z);
         glVertex3f(vertices[i + 2].x, vertices[i + 2].y, vertices[i + 2].z);
+        printf("%f, %f, %f\n", vertices[i], vertices[i + 1], vertices[i + 2]);
+        drawn++;
     }
+    cout << "Número de triangulos desenhados: " << drawn;
     glEnd();
 }
-
-/*
-
-    1 -> translate
-    2 -> rotate
-    3 -> scale
-
-    4 -> pushMatrix
-    5 -> popMatrix
-
-    6-> model
-*/
-
-void draw() {
-    int modelInd = 0;
-
-    for(int i=0 ; i<transformations.size() ; i++){
-
-        switch((int)transformations[i]) {
-
-            case 1:
-                glTranslatef(transformations[i + 1], transformations[i + 2], transformations[i + 3]);
-                i += 3;
-                printf("Translação\n");
-                break;
-
-            case 2:
-                glRotatef(transformations[i + 1], transformations[i + 2], transformations[i + 3],transformations[i + 4]);
-                i += 4;
-                printf("Rotação\n");
-                break;
-
-            case 3:
-                glScalef(transformations[i + 1], transformations[i + 2], transformations[i + 3]);
-                i += 3;
-                printf("Escala\n");
-                break;
-
-            case 4:
-                glPushMatrix();
-                printf("Push\n");
-                break;
-
-            case 5:
-                glPopMatrix();
-                printf("Pop\n");
-                break;
-
-            case 6:
-                parser(models[modelInd]);
-                modelInd++;
-                drawPrimitive();
-                vertices.clear();
-                printf("File\n");
-                break;
-        }
-    }
-
-
-}
-
-
-
-void readCamera(TiXmlElement* l_pRootElement){
-    TiXmlElement* l_camera = l_pRootElement->FirstChildElement("camera");
-
-    if (NULL != l_camera){
-
-        TiXmlElement* l_position = l_camera->FirstChildElement("position");
-
-        x1 = std::stod(l_position->Attribute("x"));
-        z1 = std::stod(l_position->Attribute("z"));
-        t1 = std::stod(l_position->Attribute("y"));
-
-        TiXmlElement* l_lookAt = l_camera->FirstChildElement("lookAt");
-
-        x2 = std::stod(l_lookAt->Attribute("x"));
-        y2 = std::stod(l_lookAt->Attribute("y"));
-        z2 = std::stod(l_lookAt->Attribute("z"));
-
-        TiXmlElement* l_up = l_camera->FirstChildElement("up");
-
-        x3 = std::stod(l_up->Attribute("x"));
-        y3 = std::stod(l_up->Attribute("y"));
-        z3 = std::stod(l_up->Attribute("z"));
-
-
-        TiXmlElement* l_proj = l_camera->FirstChildElement("projection");
-
-        fov = std::stod(l_proj->Attribute("fov"));
-        near = std::stod(l_proj->Attribute("near"));
-        far = std::stod(l_proj->Attribute("far"));
-    }
-}
-
-
-void readTransformations(TiXmlElement* l_group){
-    transformations.push_back(4.0f); // push_matrix
-
-    TiXmlElement* l_element = l_group->FirstChildElement();
-
-    while (l_element != NULL) {
-
-        if (strcmp(l_element->Value(), "transform") == 0){
-            TiXmlElement* l_transform = l_element;
-
-            TiXmlElement* l_transformation = l_transform->FirstChildElement();
-
-            while(l_transformation != NULL){
-                if (strcmp(l_transformation->Value(), "translate") == 0) {
-                    float translate_x = std::stod(l_transformation->Attribute("x"));
-                    float translate_y = std::stod(l_transformation->Attribute("y"));
-                    float translate_z = std::stod(l_transformation->Attribute("z"));
-                    transformations.push_back(1.0f);
-                    transformations.push_back(translate_x);
-                    transformations.push_back(translate_y);
-                    transformations.push_back(translate_z);
-                }
-                else if (strcmp(l_transformation->Value(), "rotate") == 0) {
-                    float angle = std::stod(l_transformation->Attribute("angle"));
-                    float rotate_x = std::stod(l_transformation->Attribute("x"));
-                    float rotate_y = std::stod(l_transformation->Attribute("y"));
-                    float rotate_z = std::stod(l_transformation->Attribute("z"));
-                    transformations.push_back(2.0f);
-                    transformations.push_back(angle);
-                    transformations.push_back(rotate_x);
-                    transformations.push_back(rotate_y);
-                    transformations.push_back(rotate_z);
-                }
-                else if (strcmp(l_transformation->Value(), "scale") == 0) {
-                    float scale_x = std::stod(l_transformation->Attribute("x"));
-                    float scale_y = std::stod(l_transformation->Attribute("y"));
-                    float scale_z = std::stod(l_transformation->Attribute("z"));
-                    transformations.push_back(3.0f);
-                    transformations.push_back(scale_x);
-                    transformations.push_back(scale_y);
-                    transformations.push_back(scale_z);
-                }
-                l_transformation = l_transformation->NextSiblingElement();
-            }
-        }
-        else if(strcmp(l_element->Value(), "group") == 0){
-            TiXmlElement* l_nested_group = l_element;
-            cout << l_nested_group->Value();
-            readTransformations(l_nested_group);
-        }else{
-            TiXmlElement* l_models = l_element;
-            TiXmlElement* l_model = l_models->FirstChildElement();
-
-            while (l_model != NULL){
-                const char* model = strdup(l_model->Attribute("file"));
-                models.push_back(model);
-                transformations.push_back(6.0f);
-                l_model = l_model->NextSiblingElement();
-            }
-
-
-        }
-        l_element = l_element->NextSiblingElement(); //avançar o elemento XML
-    }
-    transformations.push_back(5.0f); // pop_matrix
-}
-
 
 void readXML() {
 
@@ -439,19 +216,64 @@ void readXML() {
 
         if (l_pRootElement != NULL) {
 
-            readCamera(l_pRootElement);
+            TiXmlElement* l_camera = l_pRootElement->FirstChildElement("camera");
+
+            if (NULL != l_camera)
+            {
+                TiXmlElement* l_position = l_camera->FirstChildElement("position");
+
+                x1 = std::stod(l_position->Attribute("x"));
+                z1 = std::stod(l_position->Attribute("z"));
+                t1 = std::stod(l_position->Attribute("y"));
+
+                TiXmlElement* l_lookAt = l_camera->FirstChildElement("lookAt");
+
+                x2 = std::stod(l_lookAt->Attribute("x"));
+                y2 = std::stod(l_lookAt->Attribute("y"));
+                z2 = std::stod(l_lookAt->Attribute("z"));
+
+                TiXmlElement* l_up = l_camera->FirstChildElement("up");
+
+                x3 = std::stod(l_up->Attribute("x"));
+                y3 = std::stod(l_up->Attribute("y"));
+                z3 = std::stod(l_up->Attribute("z"));
+
+
+                TiXmlElement* l_proj = l_camera->FirstChildElement("projection");
+
+                fov = std::stod(l_proj->Attribute("fov"));
+                near = std::stod(l_proj->Attribute("near"));
+                far = std::stod(l_proj->Attribute("far"));
+
+            }
 
             TiXmlElement* l_group = l_pRootElement->FirstChildElement("group");
-            readTransformations(l_group);
+
+            if (NULL != l_group) {
+
+                TiXmlElement* l_models = l_group->FirstChildElement("models");
+
+                if (NULL != l_models) {
+
+                    TiXmlElement* l_model = l_models->FirstChildElement("model");
+
+                    while (l_model) {
+
+                        parser(l_model->Attribute("file"));
+                        cout << l_model->Attribute("file");
+                        l_model = l_model->NextSiblingElement("model");
+                    }
+                }
+                else {
+                    printf("models null");
+                }
+            }
         }
     }
     else {
-        printf("Ficheiro nao carregado");
+        printf("Ficheiro n�o carregado");
     }
-
-
 }
-
 
 void renderScene(void) {
 
@@ -460,30 +282,22 @@ void renderScene(void) {
 
     // set the camera
     glLoadIdentity();
-
-
-
-
-     gluLookAt(camX, camY, camZ,
-              x2, y2, z2,
-              x3, y3, z3);
+    gluLookAt(x1, t1, z1,
+        x2, y2, z2,
+        x3, y3, z3);
 
     glBegin(GL_LINES);
-    // X axis in red
-    glColor3f(1.0f, 0.0f, 0.0f);
-    glVertex3f(-100.0f, 0.0f, 0.0f);
-    glVertex3f( 100.0f, 0.0f, 0.0f);
-    // Y Axis in Green
-    glColor3f(0.0f, 1.0f, 0.0f);
-    glVertex3f(0.0f,-100.0f, 0.0f);
-    glVertex3f(0.0f, 100.0f, 0.0f);
-    // Z Axis in Blue
-    glColor3f(0.0f, 0.0f, 1.0f);
-    glVertex3f(0.0f, 0.0f,-100.0f);
-    glVertex3f(0.0f, 0.0f, 100.0f);
-    glEnd();
 
-    draw();
+
+    glColor3f(1.0f, 1.0f, 1.0f);
+    glBegin(GL_TRIANGLES);
+    for (std::size_t i = 0; i < vertices.size(); i += 3) {
+        glVertex3f(vertices[i].x, vertices[i].y, vertices[i].z);
+        glVertex3f(vertices[i + 1].x, vertices[i + 1].y, vertices[i + 1].z);
+        glVertex3f(vertices[i + 2].x, vertices[i + 2].y, vertices[i + 2].z);
+        printf("%f, %f, %f\n", vertices[i], vertices[i + 1], vertices[i + 2]);
+    }
+    glEnd();
 
 
     // End of frame
@@ -502,9 +316,7 @@ int main(int argc, char** argv) {
     filename = argv[1];
     readXML();
 
-
-    // Required callback registry
-    glutSpecialFunc(processSpecialKeys);
+    // Required callback registry 
     glutDisplayFunc(renderScene);
     glutReshapeFunc(changeSize);
 
