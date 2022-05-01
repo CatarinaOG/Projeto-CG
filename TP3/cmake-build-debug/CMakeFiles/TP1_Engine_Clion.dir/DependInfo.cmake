1ef2e0ad092b3690467278a6373a93d2689e20bd
joao <jpdelgado2001@gmail.com>
2022-05-02 00:01:00 +0100

    Fase 3 entregue

---
+++ TP3/cmake-build-debug/CMakeFiles/TP1_Engine_Clion.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/joao/git/TPs/Projeto-CG/TP3/engine/main.cpp" "CMakeFiles/TP1_Engine_Clion.dir/engine/main.cpp.o" "gcc" "CMakeFiles/TP1_Engine_Clion.dir/engine/main.cpp.o.d"
+  "/home/joao/git/TPs/Projeto-CG/TP3/engine/xml_parse.cpp" "CMakeFiles/TP1_Engine_Clion.dir/engine/xml_parse.cpp.o" "gcc" "CMakeFiles/TP1_Engine_Clion.dir/engine/xml_parse.cpp.o.d"
+  "/home/joao/git/TPs/Projeto-CG/TP3/tinystr.cpp" "CMakeFiles/TP1_Engine_Clion.dir/tinystr.cpp.o" "gcc" "CMakeFiles/TP1_Engine_Clion.dir/tinystr.cpp.o.d"
+  "/home/joao/git/TPs/Projeto-CG/TP3/tinyxml.cpp" "CMakeFiles/TP1_Engine_Clion.dir/tinyxml.cpp.o" "gcc" "CMakeFiles/TP1_Engine_Clion.dir/tinyxml.cpp.o.d"
+  "/home/joao/git/TPs/Projeto-CG/TP3/tinyxmlerror.cpp" "CMakeFiles/TP1_Engine_Clion.dir/tinyxmlerror.cpp.o" "gcc" "CMakeFiles/TP1_Engine_Clion.dir/tinyxmlerror.cpp.o.d"
+  "/home/joao/git/TPs/Projeto-CG/TP3/tinyxmlparser.cpp" "CMakeFiles/TP1_Engine_Clion.dir/tinyxmlparser.cpp.o" "gcc" "CMakeFiles/TP1_Engine_Clion.dir/tinyxmlparser.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ TP3/cmake-build-debug/CMakeFiles/TP1_Engine_Clion.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TP1_Engine_Clion.dir/engine/main.cpp.o"
+  "CMakeFiles/TP1_Engine_Clion.dir/engine/main.cpp.o.d"
+  "CMakeFiles/TP1_Engine_Clion.dir/engine/xml_parse.cpp.o"
+  "CMakeFiles/TP1_Engine_Clion.dir/engine/xml_parse.cpp.o.d"
+  "CMakeFiles/TP1_Engine_Clion.dir/tinystr.cpp.o"
+  "CMakeFiles/TP1_Engine_Clion.dir/tinystr.cpp.o.d"
+  "CMakeFiles/TP1_Engine_Clion.dir/tinyxml.cpp.o"
+  "CMakeFiles/TP1_Engine_Clion.dir/tinyxml.cpp.o.d"
+  "CMakeFiles/TP1_Engine_Clion.dir/tinyxmlerror.cpp.o"
+  "CMakeFiles/TP1_Engine_Clion.dir/tinyxmlerror.cpp.o.d"
+  "CMakeFiles/TP1_Engine_Clion.dir/tinyxmlparser.cpp.o"
+  "CMakeFiles/TP1_Engine_Clion.dir/tinyxmlparser.cpp.o.d"
+  "TP1_Engine_Clion"
+  "TP1_Engine_Clion.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TP1_Engine_Clion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
